c38ba795d1fe108c89194eaf1e71334ed939d47a
Craitz02 <rodrigocraitz02@gmail.com>
2021-04-29 07:58:08 -0600

    Conversion fxml

---
+++ src/main/java/ni/edu/uni/programacion2/conversionfx/controllers/MainFXMLController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ni.edu.uni.programacion2.conversionfx.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author JADPA26
+ */
+public class MainFXMLController implements Initializable {
+    @FXML
+    public Button btnTemperature;
+    @FXML
+    public Button btnCurrency;
+    
+        
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    public void btnTemperatureAction(){
+        
+    }
+    
+    @FXML
+    public void btnCurrencyAction(){
+        
+    }
+    
+}
