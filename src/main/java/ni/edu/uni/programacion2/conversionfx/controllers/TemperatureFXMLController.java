b2dcfd264cc32c63839103359b9bb66af64e6027
Craitz02 <rodrigocraitz02@gmail.com>
2021-04-29 19:02:26 -0600

    Temperature

--- src/main/java/ni/edu/uni/programacion2/conversionfx/controllers/TemperatureFXMLController.java
+++ src/main/java/ni/edu/uni/programacion2/conversionfx/controllers/TemperatureFXMLController.java
@@ -7,6 +7,7 @@ package ni.edu.uni.programacion2.conversionfx.controllers;
 
 
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -19,6 +20,7 @@ import javafx.scene.control.TextField;
  * @author JADPA26
  */
 public class TemperatureFXMLController implements Initializable {
+    private final DecimalFormat df = new DecimalFormat("#.0");
     @FXML
     public TextField txtCelcius;
     @FXML
@@ -37,10 +39,34 @@ public class TemperatureFXMLController implements Initializable {
     
     @FXML
     public void btnComputeAction(){
-        
+        double c,f;
+        if(txtCelcius.getText().isBlank() && txtFah.getText().isBlank()){
+            return;
+        }
+        if(txtCelcius.getText().isBlank()){
+            c=0;
+        }else{
+            c=Double.parseDouble(txtCelcius.getText());
+            txtFah.setText(String.valueOf(df.format(convAF(c))));
+        }
+        if(txtFah.getText().isBlank()){
+            f=0;
+        }else{
+            f=Double.parseDouble(txtFah.getText());
+            txtCelcius.setText(String.valueOf(df.format(convAC(f))));
+        }
     }
     @FXML
     public void btnCleanAction(){
-        
+        txtCelcius.setText("");
+        txtFah.setText("");
+    }
+    
+    private double convAC(double a) {
+        return (a - 32) * 5 / 9;
+    }
+
+    private double convAF(double a) {
+        return (a * 9 / 5) + 32;
     }
 }
